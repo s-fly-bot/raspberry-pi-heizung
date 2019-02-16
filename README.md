3c587347d71eaabf8fb443e274338fd2d1f902cd
Ralf Hense <ralf.hense@1und1.de>
2019-02-16 18:39:25 +0100

    Implement directly getting data from blnet (datalogger of uvr1611)

--- README.md
+++ README.md
@@ -14,3 +14,34 @@ Gpio 23 closes the Relais which starts the wooden fire heating.
 # Acknowledgment
 
 Thanks to Erik Bartmann for his inspiring Book "Die elektronische Welt mit Raspberry Pi entdecken"
+```
+19-01-26 00:02:24.663  INFO     aussentemperatur          : 1.6
+19-01-26 00:02:24.665  INFO     d_heizung_mischer_auf     : 0
+19-01-26 00:02:24.667  INFO     d_heizung_mischer_zu      : 0
+19-01-26 00:02:24.669  INFO     d_heizung_pumpe           : 1
+19-01-26 00:02:24.671  INFO     d_kessel_freigabe         : 0
+19-01-26 00:02:24.673  INFO     d_kessel_ladepumpe        : 0
+19-01-26 00:02:24.674  INFO     d_kessel_mischer_auf      : 0
+19-01-26 00:02:24.676  INFO     d_kessel_mischer_zu       : 0
+19-01-26 00:02:24.678  INFO     d_solar_freigabepumpe     : 0
+19-01-26 00:02:24.680  INFO     d_solar_kreispumpe        : 0
+19-01-26 00:02:24.682  INFO     d_solar_ladepumpe         : 0
+19-01-26 00:02:24.684  INFO     heizung_d                 : 30
+19-01-26 00:02:24.686  INFO     heizung_rl                : 26.6
+19-01-26 00:02:24.688  INFO     heizung_vl                : 29.4
+19-01-26 00:02:24.690  INFO     kessel_betriebstemperatur : 36.6
+19-01-26 00:02:24.692  INFO     kessel_d_ladepumpe        : 0
+19-01-26 00:02:24.694  INFO     kessel_rl                 : 27.7
+19-01-26 00:02:24.696  INFO     raum_rasp                 : 20.3
+19-01-26 00:02:24.698  INFO     solar_d_ladepumpe         : 0
+19-01-26 00:02:24.699  INFO     solar_strahlung           : 0
+19-01-26 00:02:24.701  INFO     solar_vl                  : 34
+19-01-26 00:02:24.703  INFO     speicher_1_kopf           : 75.2
+19-01-26 00:02:24.705  INFO     speicher_2_kopf           : 69
+19-01-26 00:02:24.707  INFO     speicher_3_kopf           : 37.8
+19-01-26 00:02:24.709  INFO     speicher_4_mitte          : 26.5
+19-01-26 00:02:24.711  INFO     speicher_5_boden          : 26.3
+19-01-26 00:02:24.712  INFO     speicher_ladeleitung      : 31.9
+19-01-26 00:02:24.714  INFO     timestamp                 : 1548457290
+19-01-26 00:02:24.716  INFO     datetime                  : 2019-01-26 00:01:30
+```
--- heizung.py
+++ heizung.py
@@ -4,13 +4,18 @@
 import simplejson
 import urllib2
 import datetime
-import time
-from time import gmtime, strftime
+from time import gmtime, strftime, time
 import platform
 from ConfigParser import SafeConfigParser
 import logging
 from logging import config
 import sys, os
+from htmldom import htmldom
+import requests
+import re
+import html
+import json
+
 
 raspberry = False
 if 'raspberrypi' in platform.uname():
@@ -29,16 +34,19 @@ _config_path = os.path.abspath(os.path.dirname(sys.argv[0]))
 _config_file = _config_path + "/etc/heizung.conf"
 _config_logger = _config_path+'/etc/logging.conf'
 
-print "config heizung: ", _config_file
-print "config logger : ", _config_logger
+print("config heizung: ", _config_file)
+print("config logger : ", _config_logger)
 
 parser = SafeConfigParser()
 parser.read(_config_file)
 url = parser.get('heizung', 'url')
 url_internal = parser.get('heizung', 'url_internal')
+url_blnet = parser.get('heizung', 'blnet')
+operating_mode = parser.get('heizung', 'operating_mode')
+
 log2log = parser.get('heizung', 'logger')
 
-print "print2logger  : ", log2log
+print("print2logger  : ", log2log)
 
 logging.config.fileConfig(_config_logger)
 logger = logging.getLogger('heizung')
@@ -46,43 +54,76 @@ logger.propagate = False
 
 fields = [
 ### ANALOG ####
-'timestamp',
-'kessel_rl',
-'kessel_d_ladepumpe',
-'kessel_betriebstemperatur',
-'speicher_ladeleitung',
-'aussentemperatur',
-'raum_rasp',
-'speicher_1_kopf',
-'speicher_2_kopf',
-'speicher_3_kopf',
-'speicher_4_mitte',
-'speicher_5_boden',
-'heizung_vl',
-'heizung_rl',
-'heizung_d',
-'solar_strahlung',
-'solar_vl',
-'solar_d_ladepumpe',
+    'timestamp',
+    'kessel_rl',
+    'kessel_d_ladepumpe',
+    'kessel_betriebstemperatur',
+    'speicher_ladeleitung',
+    'aussentemperatur',
+    'raum_rasp',
+    'speicher_1_kopf',
+    'speicher_2_kopf',
+    'speicher_3_kopf',
+    'speicher_4_mitte',
+    'speicher_5_boden',
+    'heizung_vl',
+    'heizung_rl',
+    'heizung_d',
+    'solar_strahlung',
+    'solar_vl',
+    'solar_d_ladepumpe',
 
 ### DIGITAL ####
-'d_heizung_pumpe',
-'d_kessel_ladepumpe',
-'d_kessel_freigabe',
-'d_heizung_mischer_auf',
-'d_heizung_mischer_zu',
-'d_kessel_mischer_auf',
-'d_kessel_mischer_zu',
-'d_solar_kreispumpe',
-'d_solar_ladepumpe',
-'d_solar_freigabepumpe']
+    'd_heizung_pumpe',
+    'd_kessel_ladepumpe',
+    'd_kessel_freigabe',
+    'd_heizung_mischer_auf',
+    'd_heizung_mischer_zu',
+    'd_kessel_mischer_auf',
+    'd_kessel_mischer_zu',
+    'd_solar_kreispumpe',
+    'd_solar_ladepumpe',
+    'd_solar_freigabepumpe']
+
+fields_dict_ein = {
+### ANALOG ####
+    '9': 'kessel_rl',
+    '7': 'kessel_betriebstemperatur',
+    '6': 'speicher_ladeleitung',
+    '1': 'aussentemperatur',
+    '8': 'raum_rasp',
+    '2': 'speicher_1_kopf',
+    '3': 'speicher_2_kopf',
+    '4': 'speicher_3_kopf',
+    '16': 'speicher_4_mitte',
+    '5': 'speicher_5_boden',
+    '10': 'heizung_vl',
+    '11': 'heizung_rl',
+    '15': 'solar_strahlung',
+    '13': 'solar_vl'
+}
+
+fields_dict_aus = {
+    '2:speed': 'kessel_d_ladepumpe',
+    '6:speed': 'heizung_d',
+    '7:speed': 'solar_d_ladepumpe',
+    '6': 'd_heizung_pumpe',
+    '2': 'd_kessel_ladepumpe',
+    '5': 'd_kessel_freigabe',
+    '10': 'd_heizung_mischer_auf',
+    '11': 'd_heizung_mischer_zu',
+    '8': 'd_kessel_mischer_auf',
+    '9': 'd_kessel_mischer_zu',
+    '4': 'd_solar_kreispumpe',
+    '3': 'd_solar_ladepumpe',
+    '7': 'd_solar_freigabepumpe'}
 
 
 def logmessage(message):
     if log2log == "True":
         logger.info(message)
     else:
-        print message
+        print(message)
 
 logmessage("+-----  S T A R T  ----------------------------------")
 logmessage("|   %r" % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
@@ -109,13 +150,15 @@ def stop_kessel():
 
 def getResonseResult(url):
     request = urllib2.Request(url)
-    # response = opener.open(request, timeout=30)
     response = urllib2.urlopen(request, timeout=30)
     response_result = response.read()
 
     return response_result
 
 def transferData():
+    """
+    This method transfers the data from uvr1611 to the api of same project to hosting server
+    """
     logmessage('+------------------ transfer data from uvr1611 ------------------------')
     try:
         if raspberry:
@@ -139,10 +182,139 @@ def transferData():
 
 
 def getMeasurementsFromHttp():
-#    response = urllib.urlopen(url)
-    data = simplejson.loads(getResonseResult(url))
-    return data[-20:]
+    response = getResonseResult(url)
+    data = ''
+    if response:
+        data = simplejson.loads(response)[-20:]
+    return data
 
+
+def getMeasurementsFromUVR1611():
+    # Ausgaenge / not only digital ones
+    r = requests.get(
+        'http://' + url_blnet + "/580600.htm",
+    )
+
+    # Parse  DOM object from HTMLCode
+    dom = htmldom.HtmlDom().createDom(r.text)
+    # get the element containing the interesting information
+    dom = dom.find("div.c")[1]
+
+    # filter out the text
+    raw = dom.text().replace('&nbsp;', '')
+
+    # build sections by id
+    sections=[]
+    section=[]
+    for line in raw.split("\n"):
+        if not line:
+            continue
+        if line[0].isdigit():
+            if section:
+                # sections.append("\n".join(section))
+                sections.append(section)
+                section=[]
+        section.append(line)
+
+    sections.append("\n".join(section))  # grab the last one
+
+    sections = sections[1:11]
+
+    ausgaenge_dict = {}
+    raw_ausgaenge_dict = {}
+
+    regex_speed      = re.compile('Drehzahlst.:(?P<speed>\d+)')
+    regex_mode_value = re.compile('(?P<mode>(AUTO|HAND))/(?P<value>(AUS|EIN))')
+    regex_auf_zu     = re.compile('(?P<mode>(auf|zu)):(?P<value>(AUS|EIN))')
+
+    for num, section in enumerate(sections):
+        #  print(num, '=' * 50)
+        first_line = True
+        speed = None
+        mode = None
+        value = None
+        for line in section:
+            if first_line:
+                # print("line : ", line)
+                id, name = line.split(':', 1)
+                first_line = False
+            if int(id) < 8:
+                match = regex_mode_value.search(line)
+                if match:
+                    # print(match.groups())
+                    mode = match.group('mode')
+                    value = match.group('value')
+            else:
+                match = regex_auf_zu.search(line)
+                if match:
+                    # print(match.groups())
+                    mode = match.group('mode')
+                    value = match.group('value')
+
+            if int(id) in [2 , 6, 7]:
+                match = regex_speed.search(line)
+                if match:
+                    speed=match.group('speed')
+
+        raw_ausgaenge_dict[id]={'id': id, 'name': name, 'mode': mode, 'value': value, 'speed': speed}
+
+    result_dict={}
+    for id in raw_ausgaenge_dict.keys():
+        if int(id) in [2, 6, 7]:
+            result_dict[fields_dict_aus[id+':speed']] = raw_ausgaenge_dict[id]['speed']
+        if raw_ausgaenge_dict[id]['value'] == 'AUS':
+            value = 0
+        else:
+            value = 1
+        result_dict[fields_dict_aus[id]] = value
+
+    """
+    Reads all analog values (temperatures, speeds) from the web interface
+    and returns list of quadruples of id, name, value, unit of measurement
+    """
+    r = requests.get(
+        'http://heizung.fritz.box/580500.htm',
+    )
+
+    # Parse  DOM object from HTMLCode
+    dom = htmldom.HtmlDom().createDom(r.text)
+    # get the element containing the interesting information
+    dom = dom.find("div.c")[1]
+    # filter out the text
+    data_raw = dom.text()
+
+    # collect data in an array
+    data = list()
+
+    # search for data by regular expression
+    match_iter = re.finditer(
+        "(?P<id>\d+):&nbsp;(?P<name>.+)\n" +
+        "(&nbsp;){3,6}(?P<value>[^ ]+) " +
+        "(?P<unit_of_measurement>.+?)&nbsp;&nbsp;PAR?", data_raw)
+    match = next(match_iter, False)
+    # parse a dict of the match and save them all in a list
+    while match:
+        match_dict = match.groupdict()
+        # convert html entities to unicode characters
+        for key in match_dict.keys():
+            match_dict[key] = html.unescape(match_dict[key])
+            # also replace decimal "," by "."
+            match_dict[key] = match_dict[key].replace(",", ".")
+        # and append formatted dict
+        data.append(match_dict)
+        match = next(match_iter, False)
+
+    for values in data:
+        result_dict[fields_dict_ein[values['id']]] = values['value']
+    return result_dict
+
+def pushDataToHosting(data):
+    """
+    Will send data to uvr1611 api to hosting server
+    :param data:
+    :return:
+    """
+    pass
 
 def getTimeDifferenceFromNow(timestamp):
     """ :return minutes from now """
@@ -150,12 +322,13 @@ def getTimeDifferenceFromNow(timestamp):
     return int(timeDiff.total_seconds() / 60)
 
 
-def check_measurements():
+def check_measurements(data=None):
     logmessage("-"*77)
     logmessage("------------- New Test on Measurements: %s -----------------" % datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     logmessage("-"*77)
 
-    data = getMeasurementsFromHttp()
+    if data is None or len(data) == 0:
+        data = getMeasurementsFromHttp()
 
     start_kessel = "--"
     start_list = {}
@@ -218,20 +391,40 @@ def main():
     if len(sys.argv) > 1 and sys.argv[1] == 'ON':
         logmessage("Start burn-off per comandline...")
         start_kessel()
-        # better wait some time?
-        time.sleep(2)
         logmessage("manually start done....")
+        time.sleep(5)
+        # better wait some time?
+        stop_kessel()
 
     else:
         while True:
+            start = time()
+            data=[]
+            try:
+                data = getMeasurementsFromUVR1611()
+                pushDataToHosting(data)
+
+            except:
+                logmessage("Unexpected error in getMeasurementsFromUVR1611(): ", sys.exc_info()[0])
+
+            # old way to transfer the data to uvr1611
+            # Todo implement api call for new "data"
             transferData()
 
-            if check_measurements() == "ON":
-                start_kessel()
-            else:
-                stop_kessel()
+            if operating_mode == 'lumber':
+                if check_measurements(data) == "ON":
+                    start_kessel()
+                else:
+                    stop_kessel()
+            elif operating_mode == 'pellets':
+                pass
 
-            time.sleep(120)  # sleeping time in seconds
+            end = time()
+
+            seconds_processing = end - start
+            to_sleep = 60 - seconds_processing
+            if seconds_processing > 0:
+                time.sleep(to_sleep)  # sleeping time in seconds
 
 if __name__ == '__main__':
     main()
